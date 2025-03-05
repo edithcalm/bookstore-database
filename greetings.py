5fbb301cb81034137412004ac936a31a33b36eda
edithcalm <edithkaranja02@gmail.com>
2025-03-05 12:50:05 +0300

    functions

---
+++ greetings.py
@@ -0,0 +1,7 @@
+# Defining a function
+def greet(name):
+    print("Hello, " + name + "!")
+
+# Calling the function
+greet("Alice")  # Outputs: Hello, Alice!
+greet("Bob")    # Outputs: Hello, Bob! 
