abf3dc32113ec7dcf2d7af775bcada64e8ec5343
edithcalm <edithkaranja02@gmail.com>
2025-04-21 23:51:15 +0300

    cross

--- SE_PLP-main/python session/forloop.py
+++
@@ -1,7 +0,0 @@
-bag_of_fruits = ["cherry", "orange", "kiwi", "melon", "mango"]
-for fruit in bag_of_fruits:
-        print(f"I love: {fruit}")
-
-    
-
-  
--- SE_PLP-main/python session/ifelse.py
+++
@@ -1,7 +0,0 @@
-temperature = 35
-
-if temperature > 30:
-    print("It's a hot day in Coast!")
-else:
-    print("It's a cool day!")
-    
